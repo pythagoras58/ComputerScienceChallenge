1a91237c87fb09ee34effa6f5b1048ad33c02308
pythagoras58 <solomondanso58@gmail.com>
2023-01-08 23:48:54 +0000

    Challenge 4 - 1

--- src/ChallengeFour.java
+++ src/ChallengeFour.java
@@ -1,2 +1,12 @@
 public class ChallengeFour {
+    /*
+    Write a function to reverse an integer using numeric
+    operators and without using any arrays or other data structures.
+    The signature of the function is:
+    int f(int n)
+     */
+
+    public static void main(String[] args) {
+
+    }
 }
