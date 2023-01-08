f3a1ed49bdb3b101f783b1085e5059912652ca16
pythagoras58 <solomondanso58@gmail.com>
2023-01-08 08:37:35 +0000

    Challenge 1 done

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Main main = new Main();
+
+        int[] myArr = {1,2,3,4};
+
+        main.isCenteredArray(myArr);
     }
 
     public int isCenteredArray(int[] isCentered){
         if((isCentered.length)%2 == 0){
+            System.out.println("NOT CENTERED");
             return 0;
         }else{
+            System.out.println("CENTERED");
             return 1;
         }
     }
