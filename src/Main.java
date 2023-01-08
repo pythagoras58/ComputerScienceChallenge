bee543a42fcc281c5cf27bbb50c2f2f37db66d24
pythagoras58 <solomondanso58@gmail.com>
2023-01-08 08:30:23 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+
+    public int isCenteredArray(int[] isCentered){
+        if((isCentered.length)%2 == 0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+}
