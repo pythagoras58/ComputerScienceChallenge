72160c99cf4c6300e36198a19a213ef5d6d21288
pythagoras58 <solomondanso58@gmail.com>
2023-01-08 22:55:58 +0000

    Challenge 3 done - question

--- src/ChallengeThree.java
+++ src/ChallengeThree.java
@@ -1,15 +1,24 @@
 public class ChallengeThree {
+
+    /*
+        Write a function that accepts a character array, a zero-based start position and a length.
+        It should return a character array containing containing lengthcharacters starting with
+        the startcharacter of the input array. The function should do error checking on the
+        start position and the length and return null if the either value is not legal.
+        The function signature is:
+        char[ ] f(char[ ] a, int start, int len)
+     */
     public static void main(String[] args) {
 
         ChallengeThree c3 = new ChallengeThree();
 
         // Test values
         char[] a = {'a', 'b', 'c'};
-        System.out.println(c3.checker(a, 0, 3));
+        System.out.println(c3.f(a, 0, 3));
 
     }
 
-    public char[] checker(char[] arr, int startPosition, int length){
+    public char[] f(char[] arr, int startPosition, int length){
         if((arr == null) || (length <0) || (startPosition <0) || (startPosition >= arr.length) || (length+startPosition > arr.length)){
             return null;
         }
