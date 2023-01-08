a464f6bb2e12ab6646c20b027f36a5f51040eb47
pythagoras58 <solomondanso58@gmail.com>
2023-01-08 22:57:27 +0000

    Challenge 2 done - question

--- src/ChallegeTwo.java
+++ src/ChallegeTwo.java
@@ -1,4 +1,10 @@
 public class ChallegeTwo {
+    /*
+    Write a function that takes an array of integers as an argument and returns a value
+    based on the sums of the even and odd numbers in the array.
+    Let X = the sum of the odd numbers in the array and let Y = the sum of the even numbers.
+    The function should return X – Y
+     */
     public static void main(String[] args) {
         ChallegeTwo challegeTwo = new ChallegeTwo();
 
