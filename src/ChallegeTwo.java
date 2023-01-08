4e0b249f174f738fd9d8a808827afaa6a83b2528
pythagoras58 <solomondanso58@gmail.com>
2023-01-08 08:49:24 +0000

    Challenge 2 done

---
+++ src/ChallegeTwo.java
@@ -0,0 +1,23 @@
+public class ChallegeTwo {
+    public static void main(String[] args) {
+        ChallegeTwo challegeTwo = new ChallegeTwo();
+
+        int[] arr = {1, 2};
+        System.out.println(challegeTwo.arrSummation(arr));
+    }
+
+    public int arrSummation(int[] arr){
+        int X = 0;
+        int Y = 0;
+
+        for(int d : arr){
+            if(d%2 == 0){
+                Y += d;
+            }else{
+                X += d;
+            }
+        }
+
+        return X - Y;
+    }
+}
