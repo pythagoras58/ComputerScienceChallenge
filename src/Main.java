a4775cf2ae9fb00b1698983f38583329a90f9c60
pythagoras58 <solomondanso58@gmail.com>
2023-01-08 22:58:30 +0000

    Challenge 1 done - question

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
+    /*
+    An array with an odd number of elements is said to be centered if all elements
+    (except the middle one) are strictly greater than the value of the middle element.
+    Note that only arrays with an odd number of elements have a middle element.
+    Write a function that accepts :
+    an integer array and returns 1 if it is a centered array, otherwise it returns 0.
+     */
     public static void main(String[] args) {
         Main main = new Main();
 
