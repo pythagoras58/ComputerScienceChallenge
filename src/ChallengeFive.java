e1586928cae2d3ffe3c2bc9a1f84f81c2bf254ca
pythagoras58 <solomondanso58@gmail.com>
2023-01-09 00:04:20 +0000

    Challenge 5 -2

--- src/ChallengeFive.java
+++ src/ChallengeFive.java
@@ -1,2 +1,16 @@
 public class ChallengeFive {
+    /*
+    Write a function to return an array containing all elements common to two given arrays containing distinct positive integers. You should not use any inbuilt methods. You are allowed to use any number of arrays.
+    The signature of the function is:
+    int[] f(int[] first, int[] second)
+    */
+
+    public static void main(String[] args) {
+        // instantiate the class
+        ChallengeFive c5 = new ChallengeFive();
+    }
+
+    int[] f(int[] first, int[] second){
+
+    }
 }
